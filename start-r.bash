d5f710a753082fa38f03d8a9139260590de0a9ca
Renan Moreira Gomes <renangomes.es@gmail.com>
2023-01-19 18:45:16 -0300

    static finished (or not)

--- start-r.bash
+++ start-r.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 scriptdir=$(dirname $0)
-router_number=2
+router_number=5
 
 for i in $(seq 1 $router_number)
 do
     echo "Starting router $i with hardware file $scriptdir/r$i-hw.txt and software file $scriptdir/r$i-sw.txt"
-    nohup java -jar $scriptdir/rtr.jar routersc r${i}-hw.txt r${i}-sw.txt > Output.out 2> Error.err < /dev/null &
+    nohup java -jar $scriptdir/rtr.jar routersc ${directory}/r${i}-hw.txt ${directory}/r${i}-sw.txt > Output.out 2> Error.err < /dev/null &
 done
