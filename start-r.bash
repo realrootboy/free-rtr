e6392ca30cbedc6980751d94c19fabf064ce3856
Renan Moreira Gomes <renangomes.es@gmail.com>
2023-01-24 17:28:33 -0300

    fix static files

--- start-r.bash
+++ start-r.bash
@@ -5,6 +5,6 @@ router_number=${routern}
 
 for i in $(seq 1 $router_number)
 do
-    echo "Starting router $i with hardware file $scriptdir/r$i-hw.txt and software file $scriptdir/r$i-sw.txt"
+    echo "Starting router $i with hardware file $directory/r$i-hw.txt and software file $scriptdir/r$i-sw.txt"
     nohup java -jar $scriptdir/rtr.jar routersc ${directory}/r${i}-hw.txt ${directory}/r${i}-sw.txt > Output.out 2> Error.err < /dev/null &
 done
